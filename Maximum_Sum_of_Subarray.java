f015990f4aef31973d2aa9ff2554431801462937
Ravin D <ravin.d3107@outlook.com>
2023-08-22 14:41:23 +0530

    Updated the code

--- Maximum_Sum_of_Subarray.java
+++ Maximum_Sum_of_Subarray.java
@@ -2,18 +2,6 @@ import java.util.*;
 
 public class Maximum_Sum_of_Subarray {
 
-
-    static int sum(int arr[])
-    {
-        int sum = 0;
-        for (int i = 0; i<arr.length; i++)
-        {
-            sum+=arr[i];
-        }
-
-        return sum;
-    }
-
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
