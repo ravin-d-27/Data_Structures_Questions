dd6d0ab0e7f6060064c976a02ca032903cff4a27
Ravin D <ravin.d3107@outlook.com>
2024-07-11 10:43:01 +0530

    Implemented Graphs using Adjacency Lists

--- DSA/Graphs/adjacencyList.py
+++ DSA/Graphs/adjacencyList.py
@@ -4,32 +4,52 @@ class Node:
         self.src = s
         self.dest = d
         self.weight = w
+        
+class Graph:
     
-
+    def __init__(self,V) -> None:
+        self.graph = dict()
+        for i in range(V):
+            self.graph[i] = []
+            
+    def insert(self, src, dest, weight):
+        self.graph[src].append(Node(src,dest,weight))
+        
+        
+    def visitNeighbours(self, neighbor):
+        for i in self.graph[neighbor]:
+            print(i.dest)
+        
+    def DFS(self, start):
+        visited = set()
+        stack = [start]
+        
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                
+                for x in self.graph[vertex]:
+                    stack.append(x.dest)
 
 if __name__=="__main__":
     V = 5
-    # Adjacency List can be implemented using dictionary
     
-    graph = dict()
+    grph = Graph(V)
     
-    for i in range(V):
-        graph[i] = []
+    grph.insert(0,1,5)
+    grph.insert(1,0,5)
+    grph.insert(1,2,1)
+    grph.insert(1,3,3)
+    grph.insert(2,1,1)
+    grph.insert(2,3,1)
+    grph.insert(2,4,2)
+    grph.insert(3,2,1)
+    grph.insert(3,1,3)
+    grph.insert(4,2,2)
     
-    graph[0].append(Node(0,1,5))
-    graph[1].append(Node(1,0,5))
-    graph[1].append(Node(1,2,1))
-    graph[1].append(Node(1,3,3))
-    graph[2].append(Node(2,1,1))
-    graph[2].append(Node(2,3,1))
-    graph[2].append(Node(2,4,2))
-    graph[3].append(Node(3,2,1))
-    graph[3].append(Node(3,1,3))
-    graph[4].append(Node(4,2,2))
-    
-    
-    #To visit Vertex 2 Neighbours:
-    
-    for i in graph[2]:
-        print(i.dest)
-    
+    grph.visitNeighbours(2)
+
+    print("\nDFS Traversal")
+    grph.DFS(0)
