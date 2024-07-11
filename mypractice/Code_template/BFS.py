d1978a17762b51c95d3e80a6794198912fd0c199
Ravin D <ravin.d3107@outlook.com>
2024-07-11 12:07:40 +0530

    Added the implementation of BFS

--- mypractice/Code_template/BFS.py
+++ mypractice/Code_template/BFS.py
@@ -1,13 +1,12 @@
 class Node:
-    
-    def __init__(self,s,d,w):
+    def __init__(self, s,d,w):
         self.src = s
         self.dest = d
         self.weight = w
         
 class Graph:
     
-    def __init__(self,V) -> None:
+    def __init__(self, V) -> None:
         self.graph = dict()
         for i in range(V):
             self.graph[i] = []
@@ -15,24 +14,20 @@ class Graph:
     def insert(self, src, dest, weight):
         self.graph[src].append(Node(src,dest,weight))
         
-        
-    def visitNeighbours(self, neighbor):
-        for i in self.graph[neighbor]:
-            print(i.dest)
-        
-    def DFS(self, start):
+    def BFS(self, start):
         visited = set()
-        stack = [start]
+        queue = [start]
         
-        while stack:
-            vertex = stack.pop()
+        while queue:
+            vertex = queue.pop(0)
+            
             if vertex not in visited:
                 visited.add(vertex)
                 print(vertex)
                 
                 for x in self.graph[vertex]:
-                    stack.append(x.dest)
-
+                    queue.append(x.dest)
+                    
 if __name__=="__main__":
     V = 5
     
@@ -48,9 +43,7 @@ if __name__=="__main__":
     grph.insert(3,2,1)
     grph.insert(3,1,3)
     grph.insert(4,2,2)
-    
-    print("\nVisting the Neighbors of 2")
-    grph.visitNeighbours(2)
 
-    print("\nDFS Traversal")
-    grph.DFS(0)
+    print("\BFS Traversal")
+    grph.BFS(0)
+        
